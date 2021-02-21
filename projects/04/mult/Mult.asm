bba36b98985cc2085aad64a10293e3ae08c0f859
Gopal Krishna Penmetsa <pgkr98@gmail.com>
2021-02-21 15:20:00 +0530

    added the changes missed in Project 4 commit.

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,28 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    @2
+    M=0
+    @i
+    M=0
+
+(LOOP)
+    @i
+    D=M
+    @0
+    D=D-M
+    @END
+    D;JGE
+
+    @1
+    D=M
+    @2
+    M=D+M
+    @i
+    M=M+1
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
